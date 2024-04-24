f226c02f7e41c5eb7dea9fb81dd6a60fd677fd10
LinhBX <Linhbxhe160309@fpt.edu.vn>
2024-04-24 18:18:32 +0700

    up

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    MONGODB_URI: process.env.MONGODB_URI,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
   },
 }
 
