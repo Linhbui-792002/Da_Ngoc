49bf57f58a49155d0b641dd5046dd9281f07d4ec
Linhbui-792002 <buixuanlinh20@gmail.com>
2024-09-09 11:16:42 +0700

    up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,21 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/66de75e150c10f7a00a607d7/1i7afoul4';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </Html>
   )
